b761dca46dff92a06f97039edbd7e0ccd84cdf65
“dam-san” <ryococokuronene@gmail.com>
2020-12-06 05:49:01 +0000

    “###”

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  
-  post 'follow/:id' => 'relationships#follow', as: 'follow' 
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
+
+  post 'follow/:id' => 'relationships#create', as: 'follow'
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
    get 'home/about' => 'homes#about'
   root 'homes#top'
-  
+
    get 'follow/:id'  => 'users#follow'
   get 'follower/:id'  => 'users#follower'
-  
+
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
-  
+
 
 end
